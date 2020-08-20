f6033582f6c1739292c7cdf71a33df965e284e9f
M.V.N.S.Prudhvi <34057003+prudhvimvns@users.noreply.github.com>
2020-08-20 15:40:14 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+
+import './answer.dart';
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget
+{
+    @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp>
+{
+
+  var _questionIndex=0;
+
+  void _answerQuestion()
+  {
+    setState(() {
+      _questionIndex=_questionIndex+1;
+    });
+    
+    print(_questionIndex);
+   
+  }
+
+
+  @override
+  Widget build(BuildContext context)
+  {
+    const questions = const [
+      {
+        'questionText':'whats\'s your fav color?', 
+      'answers':['Black','red','Green'],
+      },
+      {
+        'questionText':'What is your name?', 
+      'answers':['Pru','mvnsp','Green'],
+      },
+      
+      ];
+
+  var children2 = [
+
+    Question(questions[_questionIndex]['questionText'],),
+
+    ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
+      return Answer(_answerQuestion, answer);
+    }).toList()
+  ];
+  return MaterialApp(
+    home: Scaffold(
+  appBar: AppBar(title: Text('My First App'),),
+  body: Column(children: 
+  children2,),
+
+),);
+ 
+  }
+}
